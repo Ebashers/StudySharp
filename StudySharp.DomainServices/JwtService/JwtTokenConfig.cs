c5042ba6cfc38660276461dda45b2123054cdb5a
Nikolai Arkharov <arharovoversteam@gmail.com>
2021-07-28 23:28:54 +0300

    add SharedConfig.json file

---
+++ StudySharp.DomainServices/JwtService/JwtTokenConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace StudySharp.DomainServices.JwtService
+{
+    public sealed class JwtTokenConfig
+    {
+        public string Secret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+    }
+}
