895abc95dfc05a6e304d5d1cba9cd35bd08bb5fe
Nikolai Arkharov <45018119+KolaCation@users.noreply.github.com>
2021-09-08 16:42:29 +0300

    remove Shared project (#14)

--- server/StudySharp.Shared/Constants/AuthorizationPolicies.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StudySharp.Shared.Constants
-{
-    public static class AuthorizationPolicies
-    {
-        public static string TeacherPolicy = $"{nameof(DomainRoles.Teacher)}Policy";
-        public static string StudentPolicy = $"{nameof(DomainRoles.Student)}Policy";
-    }
-}
