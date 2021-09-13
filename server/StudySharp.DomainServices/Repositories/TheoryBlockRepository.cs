19d25b7aa4174875d05cd9a48d8fa8c111c5421b
zefirlover <ibodikmushta@gmail.com>
2021-09-13 15:32:18 +0300

    TheoryBlockRepository.cs redacted, pre-dev 1 _context is now readonly if statement and FindAsync in GetTheoryBlocksByCourseIdAsync was deleted "potentially deleted if statement" in Update was added

--- server/StudySharp.DomainServices/Repositories/TheoryBlockRepository.cs
+++ server/StudySharp.DomainServices/Repositories/TheoryBlockRepository.cs
@@ -11,22 +11,22 @@ namespace StudySharp.DomainServices.Repositories
 {
     public sealed class TheoryBlockRepository : ITheoryBlockRepository
     {
-        private StudySharpDbContext context;
+        private readonly StudySharpDbContext _context;
 
-        public TheoryBlockRepository(StudySharpDbContext _context)
+        public TheoryBlockRepository(StudySharpDbContext context)
         {
-            context = _context;
+            _context = context;
         }
         
         public async Task<OperationResult<TheoryBlock>> CreateTheoryBlockAsync(TheoryBlock theoryBlock)
         {
-            await context.TheoryBlocks.AddAsync(theoryBlock);
+            await _context.TheoryBlocks.AddAsync(theoryBlock);
             return new OperationResult<TheoryBlock>() { Result = theoryBlock, IsSucceeded = true };
         }
 
         public async Task<OperationResult<TheoryBlock>> GetTheoryBlockByIdAsync(int id)
         {
-            var theoryBlock = await context.TheoryBlocks.FindAsync(id);
+            var theoryBlock = await _context.TheoryBlocks.FindAsync(id);
             if (theoryBlock == null)
             {
                 var result = new OperationResult<TheoryBlock>() { Result = null, IsSucceeded = false };
@@ -38,33 +38,33 @@ namespace StudySharp.DomainServices.Repositories
 
         public async Task<OperationResult<List<TheoryBlock>>> GetTheoryBlocksByCourseIdAsync(int courseId)
         {
-            await context.Courses.FindAsync(courseId);
-            var theoryBlocks = await context.TheoryBlocks.Where(_ => _.CourseId == courseId).ToListAsync();
-            if (theoryBlocks == null)
-            {
-                var result = new OperationResult<List<TheoryBlock>>() { Result = null, IsSucceeded = false };
-                result.Errors.Add("There`re no matches between TheoryBlock.CourseId and courseId");
-                return result;
-            }
+            var theoryBlocks = await _context.TheoryBlocks.Where(_ => _.CourseId == courseId).ToListAsync();
             return new OperationResult<List<TheoryBlock>>(){ Result = theoryBlocks, IsSucceeded = true };
         }
 
         public async Task<OperationResult<TheoryBlock>> UpdateTheoryBlockAsync(TheoryBlock theoryBlock)
         {
-            context.Entry(theoryBlock).State = EntityState.Modified;
+            // potentially deleted if statement
+            if (theoryBlock == null)
+            {
+                var result = new OperationResult<TheoryBlock>() { Result = null, IsSucceeded = false };
+                result.Errors.Add("There`s no TheoryBlock you can modify");
+                return result;
+            }
+            _context.Entry(theoryBlock).State = EntityState.Modified;
             return new OperationResult<TheoryBlock>() { Result = theoryBlock, IsSucceeded = true };
         }
 
         public async Task<OperationResult<TheoryBlock>> RemoveTheoryBlockByIdAsync(int id)
         {
-            var theoryBlock = await context.TheoryBlocks.FindAsync(id);
+            var theoryBlock = await _context.TheoryBlocks.FindAsync(id);
             if (theoryBlock == null)
             {
                 var result = new OperationResult<TheoryBlock>() { Result = null, IsSucceeded = false };
                 result.Errors.Add("Could not find TheoryBlock`s Id");
                 return result;
             }
-            context.TheoryBlocks.Remove(theoryBlock);
+            _context.TheoryBlocks.Remove(theoryBlock);
             return new OperationResult<TheoryBlock>(){ Result = theoryBlock, IsSucceeded = true };
         }
     }
