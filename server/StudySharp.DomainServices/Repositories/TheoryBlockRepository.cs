ffa4e625471bdd55a15fd24c223374992f78befb
zefirlover <ibodikmushta@gmail.com>
2021-09-13 15:54:34 +0300

    SaveChangesAsync was added, pre-dev 3 "()" in return new OperationResult were removed too

--- server/StudySharp.DomainServices/Repositories/TheoryBlockRepository.cs
+++ server/StudySharp.DomainServices/Repositories/TheoryBlockRepository.cs
@@ -21,7 +21,8 @@ namespace StudySharp.DomainServices.Repositories
         public async Task<OperationResult<TheoryBlock>> CreateTheoryBlockAsync(TheoryBlock theoryBlock)
         {
             await _context.TheoryBlocks.AddAsync(theoryBlock);
-            return new OperationResult<TheoryBlock>() { Result = theoryBlock, IsSucceeded = true };
+            await _context.SaveChangesAsync();
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
         }
 
         public async Task<OperationResult<TheoryBlock>> GetTheoryBlockByIdAsync(int id)
@@ -29,29 +30,30 @@ namespace StudySharp.DomainServices.Repositories
             var theoryBlock = await _context.TheoryBlocks.FindAsync(id);
             if (theoryBlock == null)
             {
-                var result = new OperationResult<TheoryBlock>() { Result = null, IsSucceeded = false };
+                var result = new OperationResult<TheoryBlock> { Result = null, IsSucceeded = false };
                 result.Errors.Add("Could not find TheoryBlock`s Id");
                 return result;
             }
-            return new OperationResult<TheoryBlock>(){ Result = theoryBlock, IsSucceeded = true };
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
         }
 
         public async Task<OperationResult<List<TheoryBlock>>> GetTheoryBlocksByCourseIdAsync(int courseId)
         {
             var theoryBlocks = await _context.TheoryBlocks.Where(_ => _.CourseId == courseId).ToListAsync();
-            return new OperationResult<List<TheoryBlock>>(){ Result = theoryBlocks, IsSucceeded = true };
+            return new OperationResult<List<TheoryBlock>> { Result = theoryBlocks, IsSucceeded = true };
         }
 
         public async Task<OperationResult<TheoryBlock>> UpdateTheoryBlockAsync(TheoryBlock theoryBlock)
         {
             if (theoryBlock == null)
             {
-                var result = new OperationResult<TheoryBlock>() { Result = null, IsSucceeded = false };
+                var result = new OperationResult<TheoryBlock> { Result = null, IsSucceeded = false };
                 result.Errors.Add("There`s no TheoryBlock you can modify");
                 return result;
             }
             _context.Entry(theoryBlock).State = EntityState.Modified;
-            return new OperationResult<TheoryBlock>() { Result = theoryBlock, IsSucceeded = true };
+            await _context.SaveChangesAsync();
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
         }
 
         public async Task<OperationResult<TheoryBlock>> RemoveTheoryBlockByIdAsync(int id)
@@ -59,12 +61,13 @@ namespace StudySharp.DomainServices.Repositories
             var theoryBlock = await _context.TheoryBlocks.FindAsync(id);
             if (theoryBlock == null)
             {
-                var result = new OperationResult<TheoryBlock>() { Result = null, IsSucceeded = false };
+                var result = new OperationResult<TheoryBlock> { Result = null, IsSucceeded = false };
                 result.Errors.Add("Could not find TheoryBlock`s Id");
                 return result;
             }
             _context.TheoryBlocks.Remove(theoryBlock);
-            return new OperationResult<TheoryBlock>(){ Result = theoryBlock, IsSucceeded = true };
+            await _context.SaveChangesAsync();
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
         }
     }
 }
