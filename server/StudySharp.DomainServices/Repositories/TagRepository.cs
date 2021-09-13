fa6213344dcbcb31caab79eb30fd5a52190d80cf
Егор Павшук <egor.pavshuk@outlook.com>
2021-09-13 12:57:30 +0300

    Were realised methods in TagRepository

--- server/StudySharp.DomainServices/Repositories/TagRepository.cs
+++ server/StudySharp.DomainServices/Repositories/TagRepository.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using StudySharp.Domain.General;
 using StudySharp.Domain.Models;
 using StudySharp.Domain.Repositories;
@@ -8,24 +10,51 @@ namespace StudySharp.DomainServices.Repositories
 {
     public class TagRepository : ITagRepository
     {
-        public Task<OperationResult<Tag>> CreateTagAsync(Tag tag)
+        private readonly StudySharpDbContext _context;
+
+        public TagRepository(StudySharpDbContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+        }
+        public async Task<OperationResult<Tag>> CreateTagAsync(Tag tag)
+        {
+            await _context.Tags.AddAsync(tag);
+            await _context.SaveChangesAsync();
+            return new OperationResult<Tag>() { Result = tag, IsSucceeded = true };
         }
 
-        public Task<OperationResult<Tag>> GetTagByIdAsync(int id)
+        public async Task<OperationResult<Tag>> GetTagByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag != null)
+            {
+                return new OperationResult<Tag>() {Result = tag, IsSucceeded = true};
+            }
+            
+            var result = new OperationResult<Tag>() {Result = null, IsSucceeded = false};
+            result.Errors.Add($"Could not find the tag with id = {id}");
+            return result;
         }
 
-        public Task<OperationResult<List<Tag>>> GetTagsAsync()
+        public async Task<OperationResult<List<Tag>>> GetTagsAsync()
         {
-            throw new System.NotImplementedException();
+            var tags = await _context.Tags.ToListAsync();
+            return new OperationResult<List<Tag>>() {Result = tags, IsSucceeded = true};
         }
 
-        public Task<OperationResult<Tag>> RemoveTagByIdAsync(int id)
+        public async Task<OperationResult<Tag>> RemoveTagByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var tag = await _context.Tags.FindAsync(id);
+            if (tag != null)
+            {
+                _context.Tags.Remove(tag);
+                await _context.SaveChangesAsync();
+                return new OperationResult<Tag>() {Result = tag, IsSucceeded = true};
+            }
+
+            var result = new OperationResult<Tag>() {Result = null, IsSucceeded = false};
+            result.Errors.Add($"Could not find the tag with id = {id}");
+            return result;
         }
     }
 }
