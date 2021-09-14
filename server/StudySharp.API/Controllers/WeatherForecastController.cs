5831a3e0347fb7582666f5f43471aa6e8d23ceef
Nikolai Arkharov <arharovoversteam@gmail.com>
2021-09-14 20:48:21 +0300

    add d

--- server/StudySharp.API/Controllers/WeatherForecastController.cs
+++ server/StudySharp.API/Controllers/WeatherForecastController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -16,7 +14,7 @@ namespace StudySharp.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchingg",
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchinggg",
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
