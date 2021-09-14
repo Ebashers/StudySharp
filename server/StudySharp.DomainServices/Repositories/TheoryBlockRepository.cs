86b3acbd7fbdfbb9e46fe4e6222493481d24365e
zefirlover <71660586+zefirlover@users.noreply.github.com>
2021-09-14 20:57:32 +0300

    STD-26 | TheoryBlock Repository (#16)
    * ITheoryBlockRepository was added
    
    * TheoryBlockRepo was added
    Trying to fix it now
    
    * TheoryBlockRepository.cs was added
    I`ll try to fix it now
    
    * TheoryBlockRepository.cs done
    
    * TheoryBlockRepository.cs redacted, pre-dev 1
    _context is now readonly
    if statement and FindAsync in GetTheoryBlocksByCourseIdAsync was deleted
    "potentially deleted if statement" in Update was added
    
    * Minor changes, pre-dev 2
    "potentially deleted if statement" comment was removed
    
    * SaveChangesAsync was added, pre-dev 3
    "()" in return new OperationResult were removed too
    
    * Method AlternativeResult was added to implement DRY principle, pre-dev 4
    
    * AlternativeResult method changes, pre-dev 5
    AlternativeResult was renamed to BuildErrorResponse
    Method moved to the end of the TheoryBlockRepository.cs

---
+++ server/StudySharp.DomainServices/Repositories/TheoryBlockRepository.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudySharp.Domain.General;
+using StudySharp.Domain.Models;
+using StudySharp.Domain.Repositories;
+
+namespace StudySharp.DomainServices.Repositories
+{
+    public sealed class TheoryBlockRepository : ITheoryBlockRepository
+    {
+        private readonly StudySharpDbContext _context;
+
+        public TheoryBlockRepository(StudySharpDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<OperationResult<TheoryBlock>> CreateTheoryBlockAsync(TheoryBlock theoryBlock)
+        {
+            await _context.TheoryBlocks.AddAsync(theoryBlock);
+            await _context.SaveChangesAsync();
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
+        }
+
+        public async Task<OperationResult<TheoryBlock>> GetTheoryBlockByIdAsync(int id)
+        {
+            var theoryBlock = await _context.TheoryBlocks.FindAsync(id);
+            if (theoryBlock == null)
+            {
+                return BuildErrorResponse("Could not find TheoryBlock`s Id");
+            }
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
+        }
+
+        public async Task<OperationResult<List<TheoryBlock>>> GetTheoryBlocksByCourseIdAsync(int courseId)
+        {
+            var theoryBlocks = await _context.TheoryBlocks.Where(_ => _.CourseId == courseId).ToListAsync();
+            return new OperationResult<List<TheoryBlock>> { Result = theoryBlocks, IsSucceeded = true };
+        }
+
+        public async Task<OperationResult<TheoryBlock>> UpdateTheoryBlockAsync(TheoryBlock theoryBlock)
+        {
+            if (theoryBlock == null)
+            {
+                return BuildErrorResponse("There`s no TheoryBlock you can modify");
+            }
+            _context.Entry(theoryBlock).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
+        }
+
+        public async Task<OperationResult<TheoryBlock>> RemoveTheoryBlockByIdAsync(int id)
+        {
+            var theoryBlock = await _context.TheoryBlocks.FindAsync(id);
+            if (theoryBlock == null)
+            {
+                return BuildErrorResponse("Could not find TheoryBlock`s Id");
+            }
+            _context.TheoryBlocks.Remove(theoryBlock);
+            await _context.SaveChangesAsync();
+            return new OperationResult<TheoryBlock> { Result = theoryBlock, IsSucceeded = true };
+        }
+        
+        private OperationResult<TheoryBlock> BuildErrorResponse(string errorText)
+        {
+            var result = new OperationResult<TheoryBlock> { Result = null, IsSucceeded = false };
+            result.Errors.Add(errorText);
+            return result;
+        }
+    }
+}
