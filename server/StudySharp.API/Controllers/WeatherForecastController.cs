ad89b5c9348cba1b3e0c69de7b05ed6f2932f222
SleepyF0X <kotohomka@gmail.com>
2021-09-14 17:14:59 +0300

    Test

--- server/StudySharp.API/Controllers/WeatherForecastController.cs
+++ server/StudySharp.API/Controllers/WeatherForecastController.cs
@@ -12,7 +12,7 @@ namespace StudySharp.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchingg",
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
