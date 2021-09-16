9931f13a0a11573fbd428578f14794b6e1cf9630
Nikolai Arkharov <arharovoversteam@gmail.com>
2021-09-16 21:46:20 +0300

    move handlers to inside the queries\commands

--- server/StudySharp.ApplicationServices/Commands/AddTagCommand.cs
+++ server/StudySharp.ApplicationServices/Commands/AddTagCommand.cs
@@ -1,10 +1,37 @@
-﻿using MediatR;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 using StudySharp.Domain.General;
+using StudySharp.Domain.Models;
+using StudySharp.DomainServices;
 
 namespace StudySharp.ApplicationServices.Commands
 {
     public sealed class AddTagCommand : IRequest<OperationResult>
     {
         public string Name { get; set; }
+    }
+
+    public sealed class AddTagCommandHandler : IRequestHandler<AddTagCommand, OperationResult>
+    {
+        private readonly StudySharpDbContext _studySharpDbContext;
+
+        public AddTagCommandHandler(StudySharpDbContext studySharpDbContext)
+        {
+            _studySharpDbContext = studySharpDbContext;
+        }
+
+        public async Task<OperationResult> Handle(AddTagCommand request, CancellationToken cancellationToken)
+        {
+            if (await _studySharpDbContext.Tags.AnyAsync(_ => _.Name.ToLower().Equals(request.Name.ToLower())))
+            {
+                return OperationResult.Fail($"Tag with name [{request.Name}] already exists");
+            }
+
+            await _studySharpDbContext.Tags.AddAsync(new Tag { Name = request.Name });
+            await _studySharpDbContext.SaveChangesAsync();
+            return OperationResult.Ok();
+        }
     }
 }
--- server/StudySharp.ApplicationServices/Queries/GetTagByIdQuery.cs
+++ server/StudySharp.ApplicationServices/Queries/GetTagByIdQuery.cs
@@ -1,11 +1,35 @@
-﻿using MediatR;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
 using StudySharp.Domain.General;
 using StudySharp.Domain.Models;
+using StudySharp.DomainServices;
 
 namespace StudySharp.ApplicationServices.Queries
 {
     public sealed class GetTagByIdQuery : IRequest<OperationResult<Tag>>
     {
         public int Id { get; set; }
+    }
+
+    public sealed class GetTagByIdQueryHandler : IRequestHandler<GetTagByIdQuery, OperationResult<Tag>>
+    {
+        private readonly StudySharpDbContext _studySharpDbContext;
+
+        public GetTagByIdQueryHandler(StudySharpDbContext studySharpDbContext)
+        {
+            _studySharpDbContext = studySharpDbContext;
+        }
+
+        public async Task<OperationResult<Tag>> Handle(GetTagByIdQuery request, CancellationToken cancellationToken)
+        {
+            var tag = await _studySharpDbContext.Tags.FindAsync(request.Id);
+            if (tag == null)
+            {
+                return OperationResult.Fail<Tag>($"Could not found tag with Id [{request.Id}]");
+            }
+
+            return OperationResult.Ok(tag);
+        }
     }
 }
