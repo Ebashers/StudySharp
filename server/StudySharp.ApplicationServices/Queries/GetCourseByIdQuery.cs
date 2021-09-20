4cc4d15bc6b3371624dbc9dce0c56f693aceae9e
Egor Pavshuk <70218372+Egor-Pavshuk@users.noreply.github.com>
2021-09-20 16:00:46 +0300

    STD-34 | Course CRUD commands/queries (#29)
    * Added course CRUD commands/queries
    
    * Added cancellationToken to async methods and condition in AddCourseCommand handler
    
    * ToLower names in AddCourseCommand
    
    * Changed search method in RemoveCourseByIdCommand
    
    * Fixed condotionals in searches
    
    * Fixed tabulation
    
    Co-authored-by: Егор Павшук <egor.pavshuk@outlook.com>
    Co-authored-by: Maria Dodonova <maridodonova@gmail.com>

---
+++ server/StudySharp.ApplicationServices/Queries/GetCourseByIdQuery.cs
@@ -0,0 +1,36 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using StudySharp.Domain.Constants;
+using StudySharp.Domain.General;
+using StudySharp.Domain.Models;
+using StudySharp.DomainServices;
+
+namespace StudySharp.ApplicationServices.Queries
+{
+    public sealed class GetCourseByIdQuery : IRequest<OperationResult<Course>>
+    {
+        public int Id { get; set; }
+    }
+
+    public sealed class GetCourseByIdQueryHandler : IRequestHandler<GetCourseByIdQuery, OperationResult<Course>>
+    {
+        private readonly StudySharpDbContext _context;
+
+        public GetCourseByIdQueryHandler(StudySharpDbContext studySharpDbContext)
+        {
+            _context = studySharpDbContext;
+        }
+
+        public async Task<OperationResult<Course>> Handle(GetCourseByIdQuery request, CancellationToken cancellationToken)
+        {
+            var course = await _context.Courses.FindAsync(request.Id, cancellationToken);
+            if (course == null)
+            {
+                return OperationResult.Fail<Course>(string.Format(ErrorConstants.EntityNotFound, nameof(Course), nameof(Course.Id), request.Id));
+            }
+
+            return OperationResult.Ok(course);
+        }
+    }
+}
