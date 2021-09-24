1479528d106fbcf96f287c4f916e3fbfb6a92f6b
SleepyF0X <kotohomka@gmail.com>
2021-09-24 22:13:49 +0300

    STD-54 | Add Swager Auth functionality (#34)

--- server/StudySharp.API/Startup.cs
+++ server/StudySharp.API/Startup.cs
@@ -32,6 +32,29 @@ namespace StudySharp.API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "StudySharp.API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = @"Bearer",
+                            },
+                        },
+                        new string[] { }
+                    },
+                });
             });
 
             services.AddCors(options =>
