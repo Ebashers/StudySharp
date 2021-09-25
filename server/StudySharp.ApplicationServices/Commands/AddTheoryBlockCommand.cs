38898880a85ecb9356104c54045fa172b9b39a15
Andrew <47719935+Evenya@users.noreply.github.com>
2021-09-25 21:01:16 +0300

    STD-35 | TheoryBlock commnads/queries (#32)
    * Query Task added
    
    * created: // AddTheoryBlockCommand.cs
    // GetTheoryBlockByIdQuery.cs
    // RemoveTheoryBlockByIdCommand.cs
    
    * Fixwd conversions
    #bodyazaebal
    
    * Argument Fix
    
    * Fix Equals and code style
    
    Co-authored-by: SleepyF0X <kotohomka@gmail.com>

---
+++ server/StudySharp.ApplicationServices/Commands/AddTheoryBlockCommand.cs
@@ -0,0 +1,49 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StudySharp.Domain.Constants;
+using StudySharp.Domain.General;
+using StudySharp.Domain.Models;
+using StudySharp.DomainServices;
+
+namespace StudySharp.ApplicationServices.Commands
+{
+    public sealed class AddTheoryBlockCommand : IRequest<OperationResult>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CourseId { get; set; }
+    }
+
+    public sealed class AddTheoryBlockCommandHandler : IRequestHandler<AddTheoryBlockCommand, OperationResult>
+    {
+        private readonly StudySharpDbContext _context;
+
+        public AddTheoryBlockCommandHandler(StudySharpDbContext sharpDbContext)
+        {
+            _context = sharpDbContext;
+        }
+
+        public async Task<OperationResult> Handle(AddTheoryBlockCommand request, CancellationToken cancellationToken)
+        {
+            if (await _context.TheoryBlocks.AnyAsync(
+                _ => _.Name.ToLower().Equals(request.Name.ToLower()) && _.CourseId == request.CourseId,
+                cancellationToken))
+            {
+                return OperationResult.Fail(string.Format(ErrorConstants.EntityAlreadyExists, nameof(TheoryBlock), nameof(TheoryBlock.Name), request.Name));
+            }
+
+            await _context.TheoryBlocks.AddAsync(
+                new TheoryBlock
+                {
+                    Name = request.Name,
+                    CourseId = request.CourseId,
+                    Description = request.Description,
+                }, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return OperationResult.Ok();
+        }
+    }
+}
