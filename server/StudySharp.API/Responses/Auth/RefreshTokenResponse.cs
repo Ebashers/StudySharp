1865b51e1b10f49a2f38347f5cd29c42e338bbe4
Nikolai Arkharov <arharovoversteam@gmail.com>
2021-09-27 01:05:42 +0300

    add JwtAuthServiceExtensions, remove inheritance, change way of DI for JwtTokenConfig to use Options pattern

--- server/StudySharp.API/Responses/Auth/RefreshTokenResponse.cs
+++ server/StudySharp.API/Responses/Auth/RefreshTokenResponse.cs
@@ -1,6 +1,10 @@
 ﻿namespace StudySharp.API.Responses.Auth
 {
-    public class RefreshTokenResponse : LoginResponse
+    public sealed class RefreshTokenResponse
     {
+        public string UserName { get; set; }
+        public string Role { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
     }
 }
