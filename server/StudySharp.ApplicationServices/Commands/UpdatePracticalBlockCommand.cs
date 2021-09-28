43043aac6e42c7490fceec7b3c0170641db6f153
PavelKuzennyi <egorchikfizik@gmail.com>
2021-09-29 01:53:43 +0300

    Fixed Update

--- server/StudySharp.ApplicationServices/Commands/UpdatePracticalBlockCommand.cs
+++ server/StudySharp.ApplicationServices/Commands/UpdatePracticalBlockCommand.cs
@@ -10,7 +10,8 @@ namespace StudySharp.ApplicationServices.Commands
 {
     public sealed class UpdatePracticalBlockCommand : IRequest<OperationResult>
     {
-        public int Name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 
     public sealed class UpdatePracticalBlockCommandHandler : IRequestHandler<UpdatePracticalBlockCommand, OperationResult>
@@ -19,12 +20,13 @@ namespace StudySharp.ApplicationServices.Commands
 
         public async Task<OperationResult> Handle(UpdatePracticalBlockCommand request, CancellationToken cancellationToken)
         {
-            var practicalBlock = await _studySharpDbContext.PracticalBlocks.FindAsync(request.Name, cancellationToken);
+            var practicalBlock = await _studySharpDbContext.PracticalBlocks.FindAsync(request.Id, cancellationToken);
             if (practicalBlock == null)
             {
-                return OperationResult.Fail(string.Format(ErrorConstants.EntityNotFound, nameof(PracticalBlock), nameof(PracticalBlock.Id), request.Name));
+                return OperationResult.Fail(string.Format(ErrorConstants.EntityNotFound, nameof(PracticalBlock), nameof(PracticalBlock.Id), request.Id));
             }
 
+            practicalBlock.Name = request.Name;
             _studySharpDbContext.PracticalBlocks.Update(practicalBlock);
             await _studySharpDbContext.SaveChangesAsync(cancellationToken);
             return OperationResult.Ok();
