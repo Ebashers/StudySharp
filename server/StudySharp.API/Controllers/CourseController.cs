1f14d643a2ec7396e29c18a0cf653d29834c187e
Maria Dodonova <maridodonova@gmail.com>
2021-10-04 20:52:17 +0300

    STD-38 | Realize Course Controlleres (#33)
    * Added course controller and course requests and responses
    
    * Fixed http get/post
    
    * Added course mapper profile
    
    * Fix attributes in CourseController
    
    * Add attributes to RemoveCourseByIdRequest and to GetCourseByIdRequest models
    
    * Replace GetCourseByIdQuery by GetCoursesQuery in GetCourses method
    
    * Delete RemoveCourseByIdResponse
    
    * Fix attribute and return types in CourseController
    
    * Add GetCoursesByTeacherId query, handler, method in the controller, request and response
    
    * Add attributes in CourseController
    
    * Add validation for the existence of the teacher
    
    * Add Map for GetCoursesByTeacherId

---
+++ server/StudySharp.API/Controllers/CourseController.cs
@@ -0,0 +1,94 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudySharp.API.Requests.Courses;
+using StudySharp.API.Responses.Courses;
+using StudySharp.ApplicationServices.Commands;
+using StudySharp.ApplicationServices.Queries;
+using StudySharp.Domain.General;
+
+namespace StudySharp.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/courses")]
+
+    public class CourseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public CourseController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<OperationResult> Add([FromBody] AddCourseRequest addCourseRequest)
+        {
+            var addCourseCommand = _mapper.Map<AddCourseCommand>(addCourseRequest);
+            return await _mediator.Send(addCourseCommand);
+        }
+
+        [AllowAnonymous]
+        [HttpDelete("{id:int}")]
+        public async Task<OperationResult> Remove([FromRoute] RemoveCourseByIdRequest removeCourseByIdRequest)
+        {
+            var removeCourseByIdCommand = _mapper.Map<RemoveCourseByIdCommand>(removeCourseByIdRequest);
+            return await _mediator.Send(removeCourseByIdCommand);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id:int}")]
+        public async Task<OperationResult<GetCourseByIdResponse>> GetCourseById([FromRoute] GetCourseByIdRequest getCourseByIdRequest)
+        {
+            var getCourseByIdQuery = _mapper.Map<GetCourseByIdQuery>(getCourseByIdRequest);
+            var operationResult = await _mediator.Send(getCourseByIdQuery);
+
+            if (!operationResult.IsSucceeded)
+            {
+                return OperationResult.Fail<GetCourseByIdResponse>(operationResult.Errors);
+            }
+
+            var response = _mapper.Map<GetCourseByIdResponse>(operationResult.Result);
+            return OperationResult.Ok(response);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("~/api/teachers/{teacherId:int}/courses")]
+        public async Task<OperationResult<GetCoursesByTeacherIdResponse>> GetCoursesByTeacherId([FromRoute] GetCoursesByTeacherIdRequest getCoursesByTeacherIdRequest)
+        {
+            var getCoursesByTeacherIdQuery = _mapper.Map<GetCoursesByTeacherIdQuery>(getCoursesByTeacherIdRequest);
+            var operationResult = await _mediator.Send(getCoursesByTeacherIdQuery);
+
+            if (!operationResult.IsSucceeded)
+            {
+                return OperationResult.Fail<GetCoursesByTeacherIdResponse>(operationResult.Errors);
+            }
+
+            var response = _mapper.Map<GetCoursesByTeacherIdResponse>(operationResult.Result);
+            return OperationResult.Ok(response);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<OperationResult<GetCoursesResponse>> GetCourses([FromBody] GetCoursesRequest getCoursesRequest)
+        {
+            var getCoursesQuery = _mapper.Map<GetCoursesQuery>(getCoursesRequest);
+            var operationResult = await _mediator.Send(getCoursesQuery);
+
+            if (!operationResult.IsSucceeded)
+            {
+                return OperationResult.Fail<GetCoursesResponse>(operationResult.Errors);
+            }
+
+            var response = _mapper.Map<GetCoursesResponse>(operationResult.Result);
+            return OperationResult.Ok(response);
+        }
+    }
+}
