22b6e2427658a267103e80c6de669ba742c30019
Maria Dodonova <maridodonova@gmail.com>
2021-10-15 02:12:17 +0300

    STD-57 | Rewrite validation for PracticalBlock (#48)
    * Add PracticalBlockRules interface and methods
    
    * Rewrite validation to PracticalBlock commands
    
    * Rewrite validation for PracticalBlock queries and fix GetPracticalBlocksByCourseIdQuery
    
    * Add validation for Name and Description in UpdatePracticalBlockCommand
    
    * Fix Name length validation
    
    Co-authored-by: Егор Павшук <egor.pavshuk@outlook.com>

--- server/StudySharp.ApplicationServices/Queries/GetPracticalBlockByIdQuery.cs
+++ server/StudySharp.ApplicationServices/Queries/GetPracticalBlockByIdQuery.cs
@@ -1,9 +1,11 @@
 using System.Threading;
 using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
 using StudySharp.Domain.Constants;
 using StudySharp.Domain.General;
 using StudySharp.Domain.Models;
+using StudySharp.Domain.ValidationRules;
 using StudySharp.DomainServices;
 
 namespace StudySharp.ApplicationServices.Queries
@@ -11,6 +13,16 @@ namespace StudySharp.ApplicationServices.Queries
     public sealed class GetPracticalBlockByIdQuery : IRequest<OperationResult<PracticalBlock>>
     {
         public int Id { get; set; }
+    }
+
+    public class GetPracticalBlockByIdQueryValidator : AbstractValidator<GetPracticalBlockByIdQuery>
+    {
+        public GetPracticalBlockByIdQueryValidator(IPracticalBlockRules rules)
+        {
+            RuleFor(_ => _.Id)
+                .MustAsync((_, token) => rules.IsPracticalBlockIdExistAsync(_, token))
+                .WithMessage(_ => string.Format(ErrorConstants.EntityNotFound, nameof(PracticalBlock), nameof(PracticalBlock.Id), _.Id));
+        }
     }
 
     public sealed class GetPracticalBlockByIdQueryHandler : IRequestHandler<GetPracticalBlockByIdQuery, OperationResult<PracticalBlock>>
@@ -25,11 +37,6 @@ namespace StudySharp.ApplicationServices.Queries
         public async Task<OperationResult<PracticalBlock>> Handle(GetPracticalBlockByIdQuery request, CancellationToken cancellationToken)
         {
             var practicalBlock = await _context.PracticalBlocks.FindAsync(request.Id, cancellationToken);
-            if (practicalBlock == null)
-            {
-                return OperationResult.Fail<PracticalBlock>(string.Format(ErrorConstants.EntityNotFound, nameof(PracticalBlock), nameof(PracticalBlock.Id), request.Id));
-            }
-
             return OperationResult.Ok(practicalBlock);
         }
     }
