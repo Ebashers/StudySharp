833c6b227697d930a6bca1292eac6c5cfbfc031e
Nikolai Arkharov <arharovoversteam@gmail.com>
2021-10-16 18:00:46 +0300

    add mediatR pipeline behavior to proceed with validation

---
+++ server/StudySharp.API/Middlewares/FluentValidatorPipelineValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace StudySharp.API.Middlewares
+{
+    public class FluentValidatorPipelineValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator> _validators;
+
+        public FluentValidatorPipelineValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var validationFailures = _validators
+                .Select(validator => validator.Validate(new ValidationContext<TRequest>(request)))
+                .SelectMany(validationResult => validationResult.Errors)
+                .Where(validationFailure => validationFailure != null)
+                .ToList();
+
+            if (validationFailures.Any())
+            {
+                throw new ValidationException(validationFailures);
+            }
+
+            return next?.Invoke();
+        }
+    }
+}
